d75fe228da2393eac37e3480e2680c6f266b5dc7
Barbara <pal.barbi@gmail.com>
2021-09-01 18:51:50 +0200

    dodala kodo

---
+++ naloga10.py
@@ -0,0 +1,30 @@
+# Summation of primes
+
+#funkcije iz knjižnice eulerlib
+def sqrt(x):
+	assert x >= 0
+	i = 1
+	while i * i <= x:
+		i *= 2
+	y = 0
+	while i > 0:
+		if (y + i)**2 <= x:
+			y += i
+		i //= 2
+	return y
+
+def list_primality(n):
+	# Sieve of Eratosthenes
+	result = [True] * (n + 1)
+	result[0] = result[1] = False
+	for i in range(sqrt(n) + 1):
+		if result[i]:
+			for j in range(i * i, len(result), i):
+				result[j] = False
+	return result
+
+
+def list_primes(n):
+	return [i for (i, isprime) in enumerate(list_primality(n)) if isprime]
+
+print(sum(list_primes(1999999)))
