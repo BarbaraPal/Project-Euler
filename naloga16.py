c0fc281035ce9ffd16ce73dabf9dce7fbf06cbba
Barbara <pal.barbi@gmail.com>
2021-09-01 16:15:01 +0200

    dodala kodo

---
+++ naloga16.py
@@ -0,0 +1,9 @@
+# Power digit sum
+def vsota_stevk(n):
+    stevilo_stevk = len(str(n))
+    vsota = 0
+    for i in range(stevilo_stevk):
+        vsota += int(str(n)[i])
+    return vsota
+
+print(vsota_stevk(2**1000))
