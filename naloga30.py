566490a6b3f8635dac3ca98f92644b00c9a396a7
Barbara <pal.barbi@gmail.com>
2021-09-03 14:41:45 +0200

    dodala kodo

---
+++ naloga30.py
@@ -0,0 +1,10 @@
+# Coin sums
+def koliko():
+    nacini = [0]*201
+    nacini[0] = 1
+    for x in [1,2,5,10,20,50,100,200]:
+        for i in range(x, 201):
+            nacini[i] += nacini[i-x]
+    return nacini[200]
+
+print(koliko())
